e4d1a293a728124cc0fbf59281f04b3d4a51c383
MAPLELEAF3659 <ig910425@gmail.com>
2022-03-21 11:01:41 +0800

    Initial commit

---
+++ 8-Puzzle_Problem/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace _8_Puzzle_Problem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(340, 106);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Random Game";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button2.Location = new System.Drawing.Point(340, 190);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(159, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Start Search";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 399);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Need : ??/?? steps";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(265, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Random Generation : ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(12, 368);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(364, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Search Time : ??:??:??.??????? s";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button3.Location = new System.Drawing.Point(340, 237);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 41);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Replay";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 439);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ 8-Puzzle_Problem/Form1.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace _8_Puzzle_Problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Generate game board.
+        Button[,] gameBtn = new Button[3,3];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gameBtn[j, i] = new Button();
+                    gameBtn[j, i].Size = new Size(100,100);
+                    gameBtn[j, i].Location = new Point(15 + j * 100, 50 + i * 100);
+                    gameBtn[j, i].Text = (j + i * 3).ToString();
+                    gameBtn[j, i].Font = new Font("新細明體",20);
+                    Controls.Add(gameBtn[j, i]);
+                }
+            }
+        }
+        Random rnd = new Random();
+        int index = 0,cnt=0;
+        byte[] source = new byte[9];//Source status.
+        byte[] goal = new byte[9] {1,2,3,4,5,6,7,8,0};//Goal status.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            label3.Text = "Search Time : ??:??:??.??????? s";
+            label1.Text = "Need : ??/?? steps";
+            count = 0;
+            restart:
+            for (int i = 0; i < 9; i++)
+            {
+                source[i] = (byte)rnd.Next(9);
+                for (int j = 0; j < i; j++)
+                {
+                    if (source[i] == source[j])
+                        i--;
+                }
+            }
+            cnt = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = i + 1; j < 9; j++)
+                {
+                    if (source[i] > source[j] && source[j] != 0)
+                        cnt++;
+                }
+            }
+            //Determine if the game can be solved.
+            if (cnt % 2==1)
+            {
+                goto restart;
+            }
+            draw(source);
+        }
+
+        Stopwatch sw = new Stopwatch();//Count the search time.
+        List<Node> path = new List<Node>();//Record the shortest path.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = "Searching...";
+            button2.Enabled = false;
+            sw = Stopwatch.StartNew();
+            path = Solve(source, goal);
+            sw.Stop();
+            button2.Text = "Start Search";
+            button2.Enabled = true;
+            label3.Text = "Search Time : " + sw.Elapsed + " s";
+            label1.Text = "Need : " + "0/" + path.Count + " steps";
+            timer1.Start();
+        }
+        int count = 0;
+        //Replay the auto path.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            count = 0;
+            button3.Enabled = false;
+            timer1.Start();
+        }
+        //Play auto path.
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (count < path.Count)
+            {
+                draw(path[count++].status);
+                label1.Text = "Need : " + count + "/" + path.Count + " steps";
+            }
+            else
+            {
+                timer1.Stop();
+                button3.Enabled = true;
+            }
+        }
+        //Draw the current status.
+        private void draw(byte[] status)
+        {
+            index = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    gameBtn[j, i].Text = status[index++].ToString();
+                }
+            }
+        }
+        
+        public class Node
+        {
+            public byte[] status; // Now status
+            public Node father; // Record for previous status.If father=null,then this is root node.
+            public Node(byte[] status, Node father)
+            {
+                this.status = status;
+                this.father = father;
+            }
+
+            //Byte array to int.Ex:{1,2,3,4,5,6,7,8,0}=>123456780
+            public int ToSequence()
+            {
+                int result = 0;
+                for (int i = 0; i < status.Length; i++)
+                    result = result * 10 + status[i];
+                return result;
+            }
+        }
+        
+        List<Node> GetNext(Node now)// Get next node.
+        {
+            int index = Array.IndexOf<byte>(now.status, 0);
+            int col = index % 3;
+            int row = index / 3;
+
+            List<Node> nextPush = new List<Node>();
+            byte[] next;
+
+            if (row != 0) // Top
+            {
+                next = (byte[])now.status.Clone();
+                swap(ref next[index], ref next[index - 3]);// swap with top one.
+                nextPush.Add(new Node(next, now));// Add this status.
+            }
+
+            if (col != 2) // Right
+            {
+                next = (byte[])now.status.Clone();
+                swap(ref next[index], ref next[index + 1]);// swap with right one.
+                nextPush.Add(new Node(next, now));
+            }
+
+            if (row != 2) // Bottom
+            {
+                next = (byte[])now.status.Clone();
+                swap(ref next[index], ref next[index + 3]);// swap with bottom one.
+                nextPush.Add(new Node(next, now));
+            }
+
+            if (col != 0) // Left
+            {
+                next = (byte[])now.status.Clone();
+                swap(ref next[index], ref next[index - 1]);// swap with left one.
+                nextPush.Add(new Node(next, now));
+            }
+            return nextPush;
+        }
+        //swap two block.
+        static void swap<T>(ref T lhs, ref T rhs)
+        {
+            T temp;
+            temp = lhs;
+            lhs = rhs;
+            rhs = temp;
+        }
+        // Input source status and goal status;Output the shortest path count.
+        List<Node> Solve(byte[] source, byte[] goal)
+        {
+            Queue<Node> queue = new Queue<Node>();
+
+            // Use a sequence of states to store paths that have been taken, preventing them from going back.
+            SortedList<int, bool> book = new SortedList<int, bool>();
+
+            Node end = new Node(goal, null);// END
+            Node start = new Node(source, null);// START
+
+            queue.Enqueue(start);// Push START
+            book.Add(start.ToSequence(), true);// Mark the START that has passed,to prevent going back.
+
+            int endStatus = end.ToSequence();
+            while (queue.Count > 0)
+            {
+                // Get current search status,and pull it.
+                Node now = queue.Dequeue();
+                // If the end point is reached, then the output path.
+                if (now.ToSequence() == endStatus)
+                    return PathTrace(now);
+                
+                // Get the location that can be walked.
+                List<Node> nextPath = GetNext(now);
+                foreach (var path in nextPath)
+                {
+                    int sign = path.ToSequence();
+
+                    // Determine if the current node status has been expanded
+                    if (!book.Keys.Contains(sign))
+                    {
+                        // Push the current status into queue and mark the path has passed.                
+                        queue.Enqueue(path);
+                        book.Add(sign, true);
+                    }
+                }
+            }
+
+            // If the path is not found, the representative has no solution.
+            return null;
+        }
+        
+        List<Node> PathTrace(Node now)
+        {
+            // Trace the path.
+            List<Node> path = new List<Node>();
+            while (now.father != null)
+            {
+                path.Add(now);
+                now = now.father;
+            }
+            path.Reverse();
+            return path;
+        }
+        
+    }
+}
